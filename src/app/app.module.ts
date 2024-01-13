bff7175455eb8a363582a1aca3ecb50be20c468a
bynathan <nathancontato14112003@gmail.com>
2024-01-13 12:19:31 -0300

    I added the filter to my search field, where it will check the value entered and only show the values ​​that match.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { PokedexComponent } from './components/pokedex/pokedex.component';
+import { PokemonComponent } from './components/pokedex/pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PokedexComponent
+    PokedexComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PokeService } from 'src/app/services/poke.service';
-import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokedex',
@@ -8,39 +7,50 @@ import { mergeMap } from 'rxjs/operators';
   styleUrls: ['./pokedex.component.scss']
 })
 export class PokedexComponent implements OnInit {
-  public isFound:boolean = true;
-
   pokemons: any[] = [];
+  allPokemons: any[] = [];
+
+  public isFound:boolean = true;
 
   constructor(private pokeService: PokeService){
   }
 
   ngOnInit(): void {
     this.getPokemonDetailsList();
-    console.log(this.pokemons)
   }
 
   /**
-  * @description Obtem detalhes dos Pokemon e os adiciona a lista de Pokemon.
+  * @description Obtem o pokemon, recebe os detalhes obtidos na url do objeto pokemon, cria um novo objeto e envia para uma lista.
   * @author Nathan Silva
   */
   getPokemonDetailsList(){
     // Recebo a lista de pokemons.
     this.pokeService.getPokemons().subscribe(
       (response: any) => {
-        // Faço um "for" para todos os pokemons.
-        response.results.forEach((i: any) => {
-          // Chamo o metodo que recebe a url que tem os detalhes do pokemon.
-          this.pokeService.getUrlDetailPokemon(i.url).subscribe(
-            (detailsPokemon: object) => {
-              // Adiciono os detalhes obtido como nova propiedade do objeto de cada pokemon.
-              const pokemonDetails = { ...i, details: detailsPokemon };
-              // Envio o resultado final para um array.
-              this.pokemons.push(pokemonDetails);
-            }
-          );
-        });
+        // Faço um "map" para que pegue a url de todos os pokemons e faça um GET das informações de detalhes que contem na url.
+        response.results.map((pokemon: any) => this.pokeService.getUrlDetailPokemon(pokemon.url).subscribe(
+          (detailsPokemon: object) => {
+            // Envio o novo objeto com o pokemon e os detalhes adiquiridos na url para um array.
+            this.pokemons.push({ ...pokemon, details: detailsPokemon });
+            // Atribuo a outra variavel para utilizala novamente no filtro.
+            this.allPokemons = this.pokemons;
+          }
+        ))
       }
     )
+  }
+
+  /**
+  * @description Filtra a lista de pokémons com base no valor de pesquisa fornecido para o nome do pokémon.
+  * @author Nathan Silva
+  */
+  resultSearch(value: string) {
+    this.pokemons = this.allPokemons;
+
+    const filter = this.pokemons.filter((pokemon: any) => {
+      return pokemon.name.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+    });
+  
+    this.pokemons = filter;
   }
 }
